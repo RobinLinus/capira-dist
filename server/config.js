f9ed17d0cfd44b1d8245c2922bd0b3f4f86e785c
Robin Linus <robin_woll@capira.de>
2016-01-24 12:18:45 +0100

    Use 127.0.0.1 instead of localhost

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-    mongoDB:'mongodb://localhost:27017/capira',
+    mongoDB:'mongodb://127.0.0.1:27017/capira',
     cookieSecret: 'cookieSecret',
     lti: {
         consumerKey: 'test',
